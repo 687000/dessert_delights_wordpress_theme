683eb98d91df3a41036db33d90be9276a02fabe1
jiani liu <liujianiljn@gmail.com>
2023-05-14 22:48:07 -0700

    update homepage and 404

---
+++ 404.php
@@ -0,0 +1,10 @@
+<? get_header(); ?>
+<div class="container">
+<div class="row">
+    <div style="margin: 25vh auto; text-align:center;">
+        <h1>Oops! The page you're looking for doesn't exist.</h1>
+        <a class="btn mt-3" href="<?= get_home_url();?>">Back to Home</a>
+</div>
+</div>
+</div>
+<? get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -1,3 +1,22 @@
 <? get_header(); ?>
-<?=the_content();?>
+<div class="row">
+    <div class="left-content col-lg-3">
+        <div class="row">
+            <div class="col-lg-12 left-search-field">
+            <?
+            dynamic_sidebar('search-1');
+            ?>
+            </div>
+        </div>
+        <?
+                            dynamic_sidebar('search-2');
+        ?>
+    </div>
+    <div class="right-content col-lg-9" style="padding:0">
+        <div class="container mb-5" style="padding:0">
+        <?=the_content();?>
+        </div>
+    </div>
+</div>
+
 <? get_footer(); ?>
